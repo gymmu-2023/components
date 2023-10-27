427f953d6a56697df5de0e0526ad29a2598f7486
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-10-27 19:21:45 +0200

    Highlightjs hinzugefügt.

--- Code.jsx
+++ Code.jsx
@@ -1,14 +1,23 @@
 import {renderToString} from "react-dom/server"
 import "./components.css"
+
+import hljs from "highlight.js"
+import { useEffect, useRef } from "react"
+
 export default function Code({children}) {
+
+  const code = useRef(null)
+  useEffect(() => {
+    console.log(code.current)
+    hljs.highlightElement(code.current)
+
+  }, [])
   
   return (
-      <pre style={{
-        whiteSpace: 'pre-line'
-      }}>
-    <code>
-        {renderToString(children)}
-    </code>
+      <pre>
+        <code ref={code} className="language-html">
+          {renderToString(children)}
+        </code>
       </pre>
   )
 }

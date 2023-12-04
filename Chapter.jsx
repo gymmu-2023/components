72b250b0d6a91ca60595675ebf31cb625929fa03
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-12-04 09:03:40 +0100

    Use prism as highlighter.

--- Chapter.jsx
+++ Chapter.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+import Prism from "./prism"
+import "./prism.css"
+
 export default function Chapter({children, hash}) {
+
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
 
   return (
     <>

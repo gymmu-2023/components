e57bd4006d05c6e1216a16a21030f24caada84a3
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-08-19 12:30:41 +0000

    Usable fiddle.

--- Fiddle.jsx
+++ Fiddle.jsx
@@ -1,31 +1,53 @@
-import { useState, useRef } from "react"
+import { useState, useEffect } from "react"
 import MonacoEditor from "react-monaco-editor"
 
 import "./components.css"
 
 export default function Fiddle({ fiddleType }) {
-  const [type, setType] = useState(fiddleType)
+  const [HTMLSource, setHTMLSource] = useState("<h1>Some Title</h1>")
+  const [CSSSource, setCSSSource] = useState("")
   const [source, setSource] = useState("")
-  const outputRef = useRef(null)
 
-  const handleCodeChange = (e) => {
-    console.log(e)
-    setSource(e)
-    if (outputRef) {
-      //outputRef.current.textContent = e
-    }
+  useEffect(() => {
+    setSource(`<style>${CSSSource}</style>${HTMLSource}`)
+  }, [CSSSource, HTMLSource])
+
+  const handleHTMLChange = (e) => {
+    setHTMLSource(e)
+  }
+
+  const handleCSSChange = (e) => {
+    setCSSSource(e)
   }
 
   return (
     <>
       <div className="fiddleContainer">
-        <MonacoEditor
-          height="300px"
-          value={source}
-          onChange={handleCodeChange}
-        />
-        <div className="output" ref={outputRef}>
-          {source}
+        <div>
+          <div>
+            <h5 className="sm">HTML</h5>
+            <MonacoEditor
+              theme="vs-dark"
+              height="300px"
+              value={HTMLSource}
+              onChange={handleHTMLChange}
+            />
+          </div>
+          <div>
+            <h5 className="sm">CSS</h5>
+            <MonacoEditor
+              theme="vs-dark"
+              height="300px"
+              value={CSSSource}
+              onChange={handleCSSChange}
+            />
+          </div>
+        </div>
+        <div className="output">
+          <h5 className="sm">Webseite</h5>
+          <div
+            className="renderOutput"
+            dangerouslySetInnerHTML={{ __html: source }}></div>
         </div>
       </div>
     </>

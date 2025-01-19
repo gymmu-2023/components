ce8fa478569778e87ca5ddbc75a1ccb90e37a0c5
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-01-19 13:54:23 +0000

    Better example comonent.

--- Example.jsx
+++ Example.jsx
@@ -1,22 +1,13 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import { useRef } from "react"
 import "./components.css"
 
-export default function Example({children, title = "Beispiel"}) {
-
-  const elem = useRef(null)
-  const [index, setIndex] = useState(1)
-
-  useEffect(() => {
-    document.querySelectorAll(".example").forEach((item, i) => {
-      if (item === elem.current) setIndex(i+1)
-    })
-  }, [])
-
+export default function Example({
+  children,
+  title = "Beispiel",
+  classes = "",
+}) {
   return (
-    <div className="example" ref={elem}>
-      <h3>{`${index}. ${title}`}</h3>
+    <div className={`highlight ${classes}`}>
+      <h3>{`${title}`}</h3>
       {children}
     </div>
   )
